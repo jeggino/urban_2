e064aaead7843704c87a9d4c623cb226a108d7ba
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-08-11 09:45:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -80,9 +80,9 @@ df_model_class['price_class'] = pd.cut(df_model_class.Price,
 st.dataframe(df_model_class)
 
 
-#----------------------------------------------------------------
-df_3 = df_model_class.groupby('price_class',as_index=False).mean().round(2)
-st.dataframe(df_3)
+# #----------------------------------------------------------------
+# df_3 = df_model_class.groupby('price_class',as_index=False).mean().round(2)
+# st.dataframe(df_3)
 
 
 #----------------------------------------------------------------
