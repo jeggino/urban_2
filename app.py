0c05dff949814ecccc970abe64d34c5214a07470
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-08-11 14:57:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ st.dataframe(df_model_class)
 from sklearn import set_config
 from sklearn.utils import resample
 from sklearn.datasets import fetch_openml
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingRegressor, ExtraTreesClassifier, GradientBoostingClassifier, VotingClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingRegressor, ExtraTreesClassifier, GradientBoostingClassifier,# VotingClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.inspection import permutation_importance
 from sklearn.compose import ColumnTransformer
@@ -154,7 +154,7 @@ dict_model = {"AdaBoost Classifier":AdaBoostClassifier(),
             "Extra Trees Classifier":ExtraTreesClassifier(),
             "Gradient Boosting Classifier":GradientBoostingClassifier(),
             "Random Forest Classifier":RandomForestClassifier(),
-            "Voting Classifier":VotingClassifier()
+            # "Voting Classifier":VotingClassifier()
              }
 
 MODEL = st.selectbox(label="Chose a model", options=list(dict_model), disabled=False, label_visibility="visible")
