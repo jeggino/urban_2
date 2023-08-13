3f9622d0e3f7c381dcedf8aa83d462fb3abc360a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-08-13 23:04:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -206,9 +206,9 @@ if selecter == "Classification":
         predict = le.inverse_transform(rf.predict(df_predict))
     
         if predict == 'high':
-            st.markdown(f"The predict class is :green[HIGH] which means that the price will be ⬆️ than **_{round(df_model_class['Price'].mean())}_** **_euros_**")
+            st.subheader(f"The predict class is :green[HIGH] which means that the price will be ⬆️ than **_{round(df_model_class['Price'].mean())}_** **_euros_**")
         elif predict == 'low':
-            st.markdown(f"The predict class is :red[LOW] which means that the price will be ⬇️ than **_{round(df_model_class['Price'].mean())} euros_**")
+            st.subheader(f"The predict class is :red[LOW] which means that the price will be ⬇️ than **_{round(df_model_class['Price'].mean())} euros_**")
        
 
 
