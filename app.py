6782b757175bcb1d125fb7c46637a0cced148f7d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-08-13 21:30:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -187,10 +187,11 @@ if selecter == "Classification":
 
         return rf, le
 
-    rf, le = model()
+    model()
     
     if st.button('Fit the model with new inputs to get the price class.'):
         st.sidebar.divider()
+        rf, le = model()
         AREA = st.sidebar.slider(label="Chose area", min_value=20, max_value=150, value=30, step=1)
         ROOM = st.sidebar.slider(label="Chose rooms", min_value=1, max_value=10, value=2, step=1)
         GEBIED = st.sidebar.selectbox(label="Chose neighbour", options=df_model_class.Gebied.unique(), disabled=False, label_visibility="visible")
